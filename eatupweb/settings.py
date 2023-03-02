000ceb474cc2136c406ff47184d46fc8af26d885
horlamiedea <horlamiedea@gmail.com>
2023-03-02 11:01:06 +0100

    cors

--- eatupweb/settings.py
+++ eatupweb/settings.py
@@ -128,3 +128,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+
+CSRF_TRUSTED_ORIGINS = ['*','https://*.127.0.0.1']
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
