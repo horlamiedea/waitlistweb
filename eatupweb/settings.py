cd6936e8ad275249fa54ae170cc0ee0403b6c9f2
horlamiedea <horlamiedea@gmail.com>
2023-05-02 13:48:55 +0100

    added railway

--- eatupweb/settings.py
+++ eatupweb/settings.py
@@ -85,9 +85,13 @@ WSGI_APPLICATION = 'eatupweb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+                'ENGINE': 'django.db.backends.postgresql_psycopg2',
+                'NAME': 'railway',
+                'USER': 'postgres',
+                'PASSWORD': 'bvHesQAxx1Ma9ufFGRzt',
+                'HOST': 'containers-us-west-122.railway.app',
+                'PORT': '6077',
+            },
 }
 
 
